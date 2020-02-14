3ea014d65a4bacc7d077a34b3d3ea54b6c356673
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-14 10:42:13 -0600

    get projet actions works

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -22,4 +22,24 @@ router.get('/:id', validateID, (req, res) =>
     res.status(200).json(req.project);
 })
 
+router.get('/:id/actions', validateID, (req, res) =>
+{
+    projectDatabase.getProjectActions(req.project.id)
+    .then(actions =>
+    {
+        if(actions.length === 0)
+        {
+            res.status(404).json({error: 'No actions found for this project'})
+        }
+        else
+        {
+            res.status(200).json(actions);
+        }
+    })
+    .catch(err =>
+    {
+        res.status(500).json({error: 'Could not retrieve actions'});
+    })
+})
+
 module.exports = router;
