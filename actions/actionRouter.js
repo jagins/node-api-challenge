021326702909e5d1d2e6bdd9dda983a9164150b2
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-14 12:07:07 -0600

    get all actions works

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -1,4 +1,20 @@
 const express = require('express');
+const actionsDatabase = require('../data/helpers/actionModel');
+const {validateActions} = require('../utils');
 const router = express.Router();
+
+// /api/projects/actions
+router.get('/', (req, res) =>
+{
+    actionsDatabase.get()
+    .then(actions =>
+    {
+        res.status(200).json(actions);
+    })
+    .catch(err =>
+    {
+        res.status(500).json({error: 'Projects could not be retrieved from the database'});
+    })
+})
 
 module.exports = router;
