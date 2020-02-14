5a2e3c3f1dc82d79e4a3e19e3edcd05b7ee14c46
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-14 12:24:00 -0600

    update actions works

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -22,4 +22,24 @@ router.get('/:id', validateActionId, (req, res) =>
     res.status(200).json(req.action);
 })
 
+router.put('/:id', validateActionId, (req, res) =>
+{
+    const updateAction = {
+        project_id: req.action.project_id,
+        description: req.body.description ? req.body.description : req.action.description,
+        notes: req.body.notes ? req.body.notes : req.action.notes,
+        completed: req.body.completed ? req.body.completed : req.action.notes
+    }
+
+    actionsDatabase.update(req.action.id, updateAction)
+    .then(updatedAction =>
+    {
+        res.status(200).json(updateAction);
+    })
+    .catch(err =>
+    {
+        res.status(500).json({error: 'There was error updating the action to the database'});
+    })
+})
+
 module.exports = router;
